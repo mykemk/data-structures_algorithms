ae5fc4b23a1e5f2b0fc1989672d2d03a3dd59800
Myke <mykemitrichev@gmail.com>
2020-07-01 12:34:01 +0300

    Practiced basic data structures

---
+++ recursion/recursion.py
@@ -0,0 +1,6 @@
+'''
+- A function calls itself
+- Recursive functions need to have a breaking condition to prevent infinite loops and eventual crashes
+- Each time a function is called, the old arguments are saved
+
+'''
