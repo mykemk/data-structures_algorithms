d7c9b3d49dc9393ec39b37167216ac919cd669d2
Myke <mykemitrichev@gmail.com>
2020-07-01 06:27:04 +0300

    Examined arrays

---
+++ data_structures/arrays.py
@@ -0,0 +1,11 @@
+'''
+
+- A collection of items identified by index or key
+- Can have multiple dimensions
+***Order of Operations ***
+1. Calculating item index: O(1)
+2. Inserting or deleting items at the beggining: O(n)
+3. Inserting or deleting items on the middle: O(n)
+4. Insert or delete at the end: O(1)
+
+'''
